269442fcd166001b744fe1b35c64011f06b70cf3
DESKTOP-JDT5O80\fs <fs@imady.com>
2020-09-24 23:42:18 +0800

    啊，其实BlazorClientSample还没写呐！

--- NebuLogClientSample/NebuLogBlazorClientSample/Program.cs
+++ NebuLogClientSample/NebuLogBlazorClientSample/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
-namespace NebuLogBlazorSample.Client
+namespace NebuLogBlazorClientSample
 {
     public class Program
     {
